2ef7ac8e7269865ae54cb9b8b41b9918ba3515f7
Achraf Raouf <achraf.raouf1@etu.univ-lorraine.fr>
2026-04-28 11:17:00 +0200

    remplacer orders/# par orders/+ pour eviter reception des propres publications

--- backend/src/main/java/com/lunettes/Serveur.java
+++ backend/src/main/java/com/lunettes/Serveur.java
@@ -1,4 +1,4 @@
-﻿package com.lunettes;
+package com.lunettes;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
@@ -9,7 +9,9 @@ import org.slf4j.LoggerFactory;
 public class Serveur {
 
     private static final Logger log = LoggerFactory.getLogger(Serveur.class);
+
     private final Usine usine;
+    // final : on reçoit le client dans le constructeur et on ne le change plus jamais
     private final MqttClient mqttClient;
 
     public Serveur(Usine usine, MqttClient mqttClient) {
@@ -17,13 +19,21 @@ public class Serveur {
         this.mqttClient = mqttClient;
     }
 
+    // connecte au broker avec les options données et s'abonne aux topics
+    // si ça plante, on laisse remonter l'erreur à App.java qui décidera quoi faire
     public void demarrer(MqttConnectOptions options) throws MqttException {
+        // le callback reçoit et traite tous les messages MQTT entrants
         ServeurCallback callback = new ServeurCallback(usine, mqttClient);
         mqttClient.setCallback(callback);
+
         mqttClient.connect(options);
-        // abonnement trop large : orders/# recoit aussi nos propres publications
-        mqttClient.subscribe("orders/#");
+
+        // orders/+ : on reçoit uniquement les messages de niveau "orders/{uuid}"
+        // le "+" remplace exactement un segment, on évite orders/# qui recevrait aussi nos propres publications (orders/{uuid}/validated, /delivery, etc.)
+        mqttClient.subscribe("orders/+");
+        // serials/+/check : on reçoit les demandes de vérification de numéro de série
         mqttClient.subscribe("serials/+/check");
-        log.info("Serveur connecte, abonne a orders/# et serials/+/check");
+
+        log.info("Connecté au broker, abonné à orders/+ et serials/+/check");
     }
 }
